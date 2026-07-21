a4edcb15e94f005793e71c141e9e00f466ac6775
karthik4923 <93390048+karthik4923@users.noreply.github.com>
2026-07-21 14:12:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import plugin from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
+  base: '/Employee_Dashboard/'
     server: {
         port: 50207,
     }
